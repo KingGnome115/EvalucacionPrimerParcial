9922979dbda0a401781a4bd9c40bb624115679fa
Kevin Jesús Escutia Ceja <kevinjec115@gmail.com>
2022-09-11 17:00:42 -0500

    Quite la ActionBar pero se la deje a la galeria

--- app/src/main/java/com/example/evalucacionprimerparcial/MainActivity.java
+++ app/src/main/java/com/example/evalucacionprimerparcial/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         usuarioM = findViewById(R.id.textUsuarioMain);
         passwordM = findViewById(R.id.textPasswordMain);
 

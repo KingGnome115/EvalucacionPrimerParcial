b21740a93f794da8b8470af487bc7916e008baee
Kevin Jesús Escutia Ceja <kevinjec115@gmail.com>
2022-09-11 11:01:49 -0500

    Television ya terminada

--- app/src/main/java/com/example/evalucacionprimerparcial/Television.java
+++ app/src/main/java/com/example/evalucacionprimerparcial/Television.java
@@ -1,42 +1,69 @@
 package com.example.evalucacionprimerparcial;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
-public class Television extends AppCompatActivity {
-    private Button btnPlay, btnPause, btnStop, btnSalir;
+import java.util.ArrayList;
+
+public class Television extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
     private VideoView video;
+    private ArrayList<Integer> videoList = new ArrayList<>();
+    int currVideo = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_television);
-
         video=(VideoView) findViewById(R.id.VideoView);
-        String path = "android.resource://" + getPackageName() + "/" + R.raw.movie;
-        video.setVideoURI(Uri.parse(path));
+        video.setMediaController(new MediaController(Television.this));
+        video.setOnCompletionListener(Television.this);
 
-        btnPause = findViewById(R.id.buttonPause);
-        btnPlay = findViewById(R.id.buttonPlay);
+        videoList.add(R.raw.movie);
+        videoList.add(R.raw.movie2);
+        setVideo(videoList.get(0));
+    }
 
-        btnPause.setOnClickListener(new View.OnClickListener(){
+    private void setVideo(Integer id) {
+        String path = "android.resource://" + getPackageName() + "/" + id;
+        Uri uri = Uri.parse(path);
+        video.setVideoURI(uri);
+        video.start();
+    }
 
-            @Override
-            public void onClick(View view) {
-                video.pause();
-            }
-        });
-        btnPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        AlertDialog.Builder obj = new AlertDialog.Builder(Television.this);
+        obj.setTitle("Reproduccion terminada");
+        MyListener m = new MyListener();
+        obj.setPositiveButton("Repetir", m);
+        obj.setNegativeButton("Siguiente",m);
+        obj.setMessage("¿Siguiente?");
+        obj.show();
+    }
+
+    class MyListener implements DialogInterface.OnClickListener {
+        public void onClick(DialogInterface dialog, int which)
+        {
+            if (which == -1) {
+                video.seekTo(0);
                 video.start();
             }
-        });
+            else {
+                ++currVideo;
+                if (currVideo == videoList.size())
+                    currVideo = 0;
+                setVideo(videoList.get(currVideo));
+            }
+        }
     }
 }
